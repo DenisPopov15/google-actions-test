a02e394ccd17b3b0c56cedf022c5da016fd53e0f
Denis <denis@slatestudio.com>
2022-01-22 11:01:21 +0200

    Debugging

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const app = express()
 
 app.post('/', (req, res) => {
   console.log('Yeahh, its working')
-  console.log('req!!', req)
-  console.log('req!!', JSON.stringify(req))
+  const auth = req.headers['google-assistant-signature']
+  console.log('auth!!', auth)
   res.send('Hello World! 2022')
 })
 
