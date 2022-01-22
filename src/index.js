a5a96a75f95d6d08e4506ec04d1cc6754340561d
Denis <denis@slatestudio.com>
2022-01-22 12:52:47 +0200

    Add audience check

--- src/index.js
+++ src/index.js
@@ -12,22 +12,25 @@ const authMiddleware = (req) => {
   const jwt    = require('jsonwebtoken')
   const getPem = require('rsa-pem-from-mod-exp')
 
-  const assistantServicePublicKey = {
+  const assistantServiceJWKPublicKey = {
     e:   'AQAB',
     kty: 'RSA',
     n: 'wsM16HUl8VtOHqW-QT_k4fe67AsHPZfB05aUSIiSdPOBONDrAq7ylsVOzKRVOuCVwwmLTDL3k6fBFRYDMmhmo28AKkD6AtDA9qqkuXAovA0TFeeQ_WDHlpstE-ZgTPyykIyoG0lIMtdaZ7wKAoNv75QhtI_PAjtmJ4lCa_xx258rxr1-E8ZvUlT2I5gh_vLqYpsRuWX6uZ5Dl5SILPJuwBdfD-PIvgBGEDHT2ZpKR_IoxLNZDR7WtR6xCTiqrPj0SbMOOpyN7G28jH_jJfdw-9CD5AbWN7BlDmJU-oKcgUTee9lZLuYIPy1J7LtAac4MnGMGbupmPOQ1jzAaMpStrw'
   }
 
-  const pem = getPem(assistantServicePublicKey.n, assistantServicePublicKey.e)
+  const pem = getPem(assistantServiceJWKPublicKey.n, assistantServiceJWKPublicKey.e)
 
   const token = req.headers['google-assistant-signature']
   const decoded = jwt.verify(token, pem)
-  console.log('decoded', decoded)
+  const { iss, aud } = decoded
+  if (iss !== 'https://accounts.google.com' || aud !== 'denis-home-test') {
+    throw Error('Auth not valid')
+  }
 }
 
 app.post('/', (req, res) => {
-  console.log('Yeahh, its working')
   authMiddleware(req)
+  console.log('Yeahh, its working')
 
   res.send('Hello World! 2022')
 })
